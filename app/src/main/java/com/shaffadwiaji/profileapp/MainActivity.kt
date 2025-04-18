eba252c651b4101a32151bdd2e7c00ba1cee6e6c
ShaffaDwiaji <amagiri7141@google.com>
2025-04-18 14:21:38 +0700

    Update | Mengubah mainactivity menjadi splash screen

--- app/src/main/java/com/shaffadwiaji/profileapp/MainActivity.kt
+++ app/src/main/java/com/shaffadwiaji/profileapp/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.shaffadwiaji.profileapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.shaffadwiaji.profileapp.ui.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // Waktu tampil splash (misal 2 detik)
+    private val splashTime: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, splashTime)
     }
 }
